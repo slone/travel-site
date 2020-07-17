781b765c9de03a7aa19d4369ec05a13735f98b67
Guillaume Chantraine <guillaume.chantraine@gmail.com>
2020-07-17 09:53:46 +0200

    added modal functionality

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,28 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-let stickyHeader = new StickyHeader();
-let mobileMenu = new MobileMenu();
+new StickyHeader();
+new MobileMenu();
 let revealFeaturesOnScroll = new RevealOnScroll(document.querySelectorAll(".feature-item"), 68);
 let revealTestimonialsOnScroll = new RevealOnScroll(document.querySelectorAll(".testimonial"), 75);
 
+let modal;
+
+document.querySelectorAll(".open-modal").forEach( el => {
+	el.addEventListener("click", e => {
+		e.preventDefault();
+		if (typeof modal == "undefined") {
+			import(/* webpackChunkName: "modal" */"./modules/Modal")
+				.then( x => { 
+					modal = new x.default();
+					setTimeout( () => modal.openModal());
+				})
+				.catch( () => {	console.error('problem loading modal'); });
+		} else {
+			modal.openModal();
+		}
+	});
+})
 
 if (module.hot) {
 	module.hot.accept();
