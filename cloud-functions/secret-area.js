e09429f9bcf9e211d46f52749bf689846f64ac73
Guillaume Chantraine <guillaume.chantraine@gmail.com>
2020-07-19 21:04:11 +0200

    added CORS settings to the cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,17 @@
 exports.handler = function(event, context, callback) {
+	const headers = {
+		"Access-Control-Allow-Origin" : "*",
+		"Access-Control-Allow-Headers" : "Content-Type"
+	}	
+
+	if (event.httpMethod !== "POST") {
+		return callback(null, {
+			statusCode: 200,
+			headers,
+			body: "This was not a POST request",
+		});
+	}
+	
 	const secretContent = `
 		<h3>Welcome to the secret area</h3>
 		<p>Here, we can tell you that the sky is <strong>blue</strong> and two plus two equals four.</p>
